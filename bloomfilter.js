faf11c1fcc49f1a08d36d0a43facfaa5787cc78a
Zaz Brown <zazbrown@yandex.com>
2016-07-13 12:03:19 +0000

    Rewrite half of the program

--- bloomfilter.js
+++ bloomfilter.js
@@ -1,118 +1,93 @@
 (function(exports) {
   exports.BloomFilter = BloomFilter;
-  exports.fnv_1a = fnv_1a;
-  exports.fnv_1a_b = fnv_1a_b;
+  exports.hash_a = hash_a;
+  exports.hash_b = hash_b;
 
-  var typedArrays = typeof ArrayBuffer !== "undefined";
+  // Creates a new bloom filter.
+  // *a* is the array used to initialize the bloom filter.
+  //     For an empty bloom filter use:  a = new Array(n).fill(0)
+  // *k* is the number of hash functions.
+  class BloomFilter {
+    constructor(a, k) {
+      var n = a.length
+      this.m = n * 32
+      this.k = k
+      this.buckets = a
+    }
 
-  // Creates a new bloom filter.  If *m* is an array-like object, with a length
-  // property, then the bloom filter is loaded with data from the array, where
-  // each element is a 32-bit integer.  Otherwise, *m* should specify the
-  // number of bits.  Note that *m* is rounded up to the nearest multiple of
-  // 32.  *k* specifies the number of hashing functions.
-  function BloomFilter(m, k) {
-    var a;
-    if (typeof m !== "number") a = m, m = a.length * 32;
+    // See http://willwhim.wpengine.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
+    locations(v) {
+      var m = this.m,
+          a = hash_a(String(v)),
+          b = mod(hash_b(a), m),
+          a = mod(a, m),
+          l = []
+      for (var i = 0; i < this.k; ++i) {
+        l[i] = a
+        a = (a + b) % m
+      }
+      return l
+    }
 
-    var n = Math.ceil(m / 32),
-        i = -1;
-    this.m = m = n * 32;
-    this.k = k;
+    // Set ith bit to 1
+    set(i) { this.buckets[Math.floor(i/32)] |= 1 << i }
+    // Check if ith bit is 1
+    get(i) { return this.buckets[Math.floor(i/32)] & (1 << (i % 32)) }
 
-    if (typedArrays) {
-      var kbytes = 1 << Math.ceil(Math.log(Math.ceil(Math.log(m) / Math.LN2 / 8)) / Math.LN2),
-          array = kbytes === 1 ? Uint8Array : kbytes === 2 ? Uint16Array : Uint32Array,
-          kbuffer = new ArrayBuffer(kbytes * k),
-          buckets = this.buckets = new Int32Array(n);
-      if (a) while (++i < n) buckets[i] = a[i];
-      this._locations = new array(kbuffer);
-    } else {
-      var buckets = this.buckets = [];
-      if (a) while (++i < n) buckets[i] = a[i];
-      else while (++i < n) buckets[i] = 0;
-      this._locations = [];
+    add(v) { this.locations(v).forEach(this.set, this) }
+    test(v) { return this.locations(v).every(this.get, this) }
+
+    // Estimated cardinality.
+    size() {
+      var bits = 0
+      for (let b of this.buckets) bits += popcnt(b)
+      return -this.m * Math.log(1 - bits / this.m) / this.k
     }
   }
 
-  // See http://willwhim.wpengine.com/2011/09/03/producing-n-hash-functions-by-hashing-only-once/
-  BloomFilter.prototype.locations = function(v) {
-    var k = this.k,
-        m = this.m,
-        r = this._locations,
-        a = fnv_1a(v),
-        b = fnv_1a_b(a),
-        x = a % m;
-    for (var i = 0; i < k; ++i) {
-      r[i] = x < 0 ? (x + m) : x;
-      x = (x + b) % m;
-    }
-    return r;
-  };
+  // Return true (positive) modulus of x
+  function mod(x, m) {
+    return ((x % m) + m) %m
+  }
 
-  BloomFilter.prototype.add = function(v) {
-    var l = this.locations(v + ""),
-        k = this.k,
-        buckets = this.buckets;
-    for (var i = 0; i < k; ++i) buckets[Math.floor(l[i] / 32)] |= 1 << (l[i] % 32);
-  };
-
-  BloomFilter.prototype.test = function(v) {
-    var l = this.locations(v + ""),
-        k = this.k,
-        buckets = this.buckets;
-    for (var i = 0; i < k; ++i) {
-      var b = l[i];
-      if ((buckets[Math.floor(b / 32)] & (1 << (b % 32))) === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  // Estimated cardinality.
-  BloomFilter.prototype.size = function() {
-    var buckets = this.buckets,
-        bits = 0;
-    for (var i = 0, n = buckets.length; i < n; ++i) bits += popcnt(buckets[i]);
-    return -this.m * Math.log(1 - bits / this.m) / this.k;
-  };
 
   // http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetParallel
   function popcnt(v) {
-    v -= (v >> 1) & 0x55555555;
-    v = (v & 0x33333333) + ((v >> 2) & 0x33333333);
-    return ((v + (v >> 4) & 0xf0f0f0f) * 0x1010101) >> 24;
+    v -= (v >> 1) & 0x55555555
+    v = (v & 0x33333333) + ((v >> 2) & 0x33333333)
+    return ((v + (v >> 4) & 0xf0f0f0f) * 0x1010101) >> 24
   }
 
-  // Fowler/Noll/Vo hashing.
-  function fnv_1a(v) {
-    var a = 2166136261;
+  // Fowler/Noll/Vo (FNV) hash function
+  function hash_a(v) {
+    var a = 2166136261
     for (var i = 0, n = v.length; i < n; ++i) {
       var c = v.charCodeAt(i),
-          d = c & 0xff00;
-      if (d) a = fnv_multiply(a ^ d >> 8);
-      a = fnv_multiply(a ^ c & 0xff);
+          d = c & 0xff00
+      if (d) a = fnv_multiply(a ^ d >> 8)
+      a = fnv_multiply(a ^ c & 0xff)
     }
-    return fnv_mix(a);
+    return fnv_mix(a)
+  }
+
+  // One additional iteration of FNV, given a hash.
+  function hash_b(a) {
+    return fnv_mix(fnv_multiply(a))
   }
 
   // a * 16777619 mod 2**32
   function fnv_multiply(a) {
-    return a + (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24);
-  }
-
-  // One additional iteration of FNV, given a hash.
-  function fnv_1a_b(a) {
-    return fnv_mix(fnv_multiply(a));
+    return a + (a << 1) + (a << 4) + (a << 7) + (a << 8) + (a << 24)
   }
 
   // See https://web.archive.org/web/20131019013225/http://home.comcast.net/~bretm/hash/6.html
   function fnv_mix(a) {
-    a += a << 13;
-    a ^= a >>> 7;
-    a += a << 3;
-    a ^= a >>> 17;
-    a += a << 5;
-    return a & 0xffffffff;
+    a += a << 13
+    a ^= a >>> 7
+    a += a << 3
+    a ^= a >>> 17
+    a += a << 5
+    return a & 0xffffffff
   }
-})(typeof exports !== "undefined" ? exports : this);
+
+})
